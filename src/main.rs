db8f746952e882fb927a8c82d2215746cfc7c010
Faruk <farukomererdem2003@gmail.com>
2025-02-27 21:40:12 +0300

    initial

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+};
+use ratatui::{
+    Terminal,
+    backend::CrosstermBackend,
+    widgets::{Block, Borders, Paragraph},
+};
+use std::io;
+
+fn main() -> Result<(), io::Error> {
+    // Setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // App loop
+    let res = run_app(&mut terminal);
+
+    // Restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err);
+    }
+
+    Ok(())
+}
+
+fn run_app<B: ratatui::backend::Backend>(terminal: &mut Terminal<B>) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| {
+            let size = f.size();
+            let block = Block::default().title("My TUI App").borders(Borders::ALL);
+            let text =
+                Paragraph::new("Press 'q' to quit.").block(Block::default().borders(Borders::ALL));
+
+            f.render_widget(block, size);
+            f.render_widget(text, size);
+        })?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.code == KeyCode::Char('q') {
+                return Ok(());
+            }
+        }
+    }
+}
+
