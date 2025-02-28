8aa07b165ca0c960762d640e1b6f2bc08ed5a441
Faruk <farukomererdem2003@gmail.com>
2025-02-28 22:07:58 +0300

    Stower

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,6 @@
 use git2::Repository;
 use std::env;
-
+use std::path::Path;
 use std::process::Command;
 
 pub struct App {
@@ -16,6 +16,7 @@ pub enum MenuItem {
     CloneRepo,
     UpgradePackages,
     InstallPackages,
+    LinkDotFiles,
     Quit,
 }
 
@@ -28,6 +29,7 @@ impl App {
                 ("Upgrade Packages", MenuItem::UpgradePackages),
                 ("Install Packages", MenuItem::InstallPackages),
                 ("Clone Repository", MenuItem::CloneRepo),
+                ("Link Dotfiles", MenuItem::LinkDotFiles),
                 ("Quit", MenuItem::Quit),
             ],
             output: String::from("Welcome! Select an option and press Enter to execute."),
@@ -66,9 +68,22 @@ impl App {
             MenuItem::CloneRepo => self.clone_repository(),
             MenuItem::UpgradePackages => self.upgrade_packages(),
             MenuItem::InstallPackages => self.install_packages(),
+            MenuItem::LinkDotFiles => self.link_dot_files(),
             MenuItem::Quit => {}
         }
     }
+
+    fn is_command_exist(&mut self, cmd: &str, checker: Option<&str>) -> bool {
+        let checker = checker.unwrap_or("--version");
+        let output = Command::new(&cmd).arg(&checker).output();
+        let is_exist;
+        match output {
+            Ok(_) => is_exist = true,
+            _ => is_exist = false,
+        }
+        is_exist
+    }
+
     fn get_package_manager(&mut self) -> String {
         let mut package_manager = String::new();
         let apt_check = Command::new("dpkg").arg("--version").output();
@@ -121,10 +136,13 @@ impl App {
             Err(e) => self.output = String::from(format!("{}", e)),
         }
     }
-
+    fn get_home_directory(&mut self) -> String {
+        let home_folder = env::var("HOME").expect("$HOME envirenment variable is not set!");
+        home_folder
+    }
     fn clone_repo(&mut self) -> String {
         let repo_url = String::from("https://github.com/farukerdem34/dotfiles.git");
-        let home_folder = env::var("HOME").expect("$HOME envirenment variable is not set!");
+        let home_folder = self.get_home_directory();
         let clone_path = format!("{}/.dotfiles", &home_folder);
 
         match Repository::clone(&repo_url, &clone_path) {
@@ -231,5 +249,44 @@ impl App {
             }
             _ => self.output = String::from("No valid package manager found!"),
         };
+    }
+    fn link_dot_files(&mut self) {
+        if self.is_command_exist("stow", Some("--version")) {
+            let home = self.get_home_directory();
+            let work_path;
+            if Path::new(format!("{}/dotfiles", home).as_str()).is_dir() {
+                work_path = format!("{}/dotfiles", home);
+            } else if Path::new(format!("{}/dotfiles", home).as_str()).is_dir() {
+                work_path = format!("{}/.dotfiles", home);
+            } else {
+                self.output = String::from("Dotfiles directory not found in $HOME/.dotfiles");
+                panic!("Dotfiles directory not found in $HOME/.dotfiles");
+            }
+
+            let mut cmd = Command::new("stow");
+            cmd.current_dir(work_path);
+            let packages = vec![
+                "bash",
+                "bat",
+                "btop",
+                "fastfetch",
+                "kitty",
+                "nvim",
+                "starship",
+                "tmux",
+                "vimrc",
+                "zsh",
+            ];
+            for pkg in packages {
+                cmd.arg(pkg);
+            }
+            let output = cmd.output();
+            match output {
+                Ok(_) => self.output = String::from("Packages stowed succesfully."),
+                Err(e) => self.output = format!("{}", e),
+            }
+        } else {
+            self.output = String::from("Stow package doesn't exists.")
+        }
     }
 }
